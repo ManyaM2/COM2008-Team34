bdcc4996291a5b3ca00f48f6999b82201ec6114c
Randi Chandrsena <rschandrasena1@sheffield.ac.uk>
2023-11-08 19:55:19 +0000

    Account, Address, User and BankDetails classes set up

---
+++ Account_User_Bank_Details/src/User.java
@@ -0,0 +1,10 @@
+public class User {
+
+    private String userForename;
+    private String userSurname;
+
+    public User (String forename, String surname) {
+        this.userForename = forename;
+        this.userSurname = surname;
+    }
+}
