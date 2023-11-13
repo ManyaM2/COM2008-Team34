ccf517c9b3148b3c12a1a148fc5c89e71852fcca
Jay-Harrison2004 <iharrison1@sheffield.ac.uk>
2023-11-13 12:42:52 +0000

    Combined the user and account classes

--- TrainsOfSheffield_Team34/src/User.java
+++ TrainsOfSheffield_Team34/src/User.java
@@ -1,7 +1,23 @@
-public class User {
+import java.util.List;
 
+public class User {
+    private int userID;
+    private String userEmail;
+    private List<String> userRoles;
     private String userForename;
     private String userSurname;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public List<String> getUserRoles() {
+        return userRoles;
+    }
 
     public String getUserForename() {
         return userForename;
@@ -11,8 +27,11 @@ public class User {
         return userSurname;
     }
 
-    public User (String forename, String surname) {
+    public User (String forename, String surname, int id, String email, List<String> roles) {
         this.userForename = forename;
         this.userSurname = surname;
+        this.userID = id;
+        this.userEmail = email;
+        this.userRoles = roles;
     }
 }
